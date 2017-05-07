801fe5ddb31cef4c3bfd7418cb51a450371da1a5
Толмачева Марина <tolmacheva.ma@gmail.com>
2017-05-07 16:36:18 +1000

    Задание 8

--- LAB/Less4_Lab8.js
+++ LAB/Less4_Lab8.js
@@ -7,48 +7,88 @@ var chrome = require('selenium-webdriver/chrome');
 var By = webdriver.By;
 var until = webdriver.until;
 var test = require('selenium-webdriver/testing');
+var promise = require('selenium-webdriver').promise;
 var assert = require('assert');
-
+var linlkLength,  stickLength;
 
 test.describe('Check Main Left menu', function() {
     var driver;
+    //var linlkLength,  stickLength;
 
     test.before(function() {
         driver = new webdriver.Builder()
             .withCapabilities({'unexpectedAlertBehaviour': 'dismiss'})
             .forBrowser('chrome')
             .build();
-        driver.manage().timeouts().implicitlyWait(10000/*ms*/);
 
         driver.get('http://localhost/litecart/');
+        driver.manage().timeouts().implicitlyWait(10000/*ms*/);
+
     });
+
+/*
+    test.it('Проверяем', function() {
+        driver.findElements(By.css("#content > ul > li > a")).then(function (tabElems) {
+            var j=1;
+            for (i = 0; i < tabElems.length; i++)
+            {
+                var path1 = "//div[@class='tab-content']/div[" + j + "]//a";
+                console.log(path1, "path1");
+                var path2 = "//div[@class='tab-content']/div[" + j + "]//a//div[contains(@class, 'sticker')]";
+                console.log(path2, "path2");
+ driver.manage().timeouts().implicitlyWait(10000/!*ms*!/);
+ linlkLength = driver.findElements(By.xpath(path1)).length;
+ driver.manage().timeouts().implicitlyWait(10000/!*ms*!/);
+ stickLength = driver.findElements(By.xpath(path2)).length;
+ driver.manage().timeouts().implicitlyWait(10000/!*ms*!/);
+                tabElems[i].click();
+
+
+                console.log(linlkLength, "linlkLength");
+                console.log(stickLength, "stickLength");
+                j++;
+            }
+        });
+    });
+*/
 
     test.it('Проверяем', function() {
+        driver.findElements(By.css("#content > ul > li > a")).then(function (tabElems) {
+            i=1;
+            tabElems.forEach(function (elemTab) {
+                
+                var path1 = "//div[@class='tab-content']/div[" + i + "]//a";
+                var path2 = "//div[@class='tab-content']/div[" + i + "]//a//div[contains(@class, 'sticker')]";
 
-        var elemTab = driver.findElements(By.xpath("//*[@id='content']/ul/li/a"));
+                elemTab.click();
+                driver.manage().timeouts().implicitlyWait(5000/*ms*/);
+                driver.findElement(By.css("#content > ul > li.active > a")).then(function (elem) {
+                    return elem.getAttribute("hash");
+                }).then(function (elem) {
+                    driver.manage().timeouts().implicitlyWait(5000/*ms*/);
+                    driver.findElements(By.xpath(path1)).then(function (elLinks) {
+                        return linlkLength = elLinks.length;
+                    }).then(function (elem) {
+                        driver.manage().timeouts().implicitlyWait(5000/*ms*/);
+                        driver.findElements(By.xpath(path2)).then(function (elSticks) {
+                            return stickLength = elSticks.length;
+                        })
+                    }).then(function () {
+                        assert.equal(linlkLength,stickLength);
+                        //console.log(linlkLength, "linlkLength")
+                        //console.log(stickLength, "stickLength");
 
-        // Клик по меню
-        for (var i = 0; i < elemTab.length; i++) {
-            var elem = driver.findElement(By.xpath("//!*[@id='content']/ul/li["+i+"]/a")).Click();
-            var id =elem.hash;
-
-            var elemLink = driver.findElements(By.css( elem.hash.toString() + " .products .product a"));
-            var elemSticker = driver.findElements(By.css( elem.hash.toString() + " .products .product .sticker"));
-            if (elemLink.length = elemSticker.length)
-                assert.ok(elemSticker.length, "")
-
-        }
+                    });
+                });
+                //assert.equal(linlkLength,stickLength);
+                i++
+/*
+                console.log(linlkLength, "linlkLength")
+                console.log(stickLength, "stickLength");
+*/
+            });
+        });
     });
-
-    test.it('Проверяем 2', function() {
-
-        var elemLink = driver.findElements(By.css(".products .product a"));
-        var elemSticker = driver.findElements(By.css(".products .product .sticker"));
-        if (elemLink.length = elemSticker.length) {
-            assert.ok(elemSticker.length, "")
-        }
-    });
-
 
     test.after(function() {
         driver.quit();
