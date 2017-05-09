8ea3dc330d266a26e3869b3ecf9c11a9714cbe6c
Толмачева Марина <tolmacheva.ma@gmail.com>
2017-05-09 19:42:51 +1000

    Задание 10

--- LAB/less5_lab10FF.js
+++ LAB/less5_lab10FF.js
@@ -1,24 +1,24 @@
-// Задание 10
+// Задание 10 Firefox
 // Войти на главную страницу http://localhost/litecart
 // Проверить наименование товара, цены товара (обычная, аукционная)
 // Проверить стиль цены товара (обычная, аукционная)
 
 var webdriver = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
 var By = webdriver.By;
 var until = webdriver.until;
 var test = require('selenium-webdriver/testing');
 var assert = require('assert');
 var $q = require('q');
 
-test.describe('Проверка стиля в Chrome', function () {
+test.describe('Проверка стиля в Firefox', function () {
     var driver;
 
     test.before(function () {
         driver = new webdriver.Builder()
-            .withCapabilities({'unexpectedAlertBehaviour': 'dismiss'})
-            .forBrowser('chrome')
+            .withCapabilities({'marionette': true})
+            .forBrowser('firefox')
             .build();
+
 
         driver.get('http://localhost/litecart/');
         driver.manage().timeouts().implicitlyWait(10000/*ms*/);
@@ -49,7 +49,7 @@ test.describe('Проверка стиля в Chrome', function () {
 
                 elem.getCssValue("color").then(function (attr) {
                     var str = attr.split("(")[1].split(",",3);
-                    assert.ok( str[0]&&str[1]&&str[2], "Проверка - цвет основной цены на карточке");
+                    assert.ok( str[0]&&str[1]&&str[2], "Проверка - цвет основной цены  на карточке");
                     return attr; })
 
                 elem.getCssValue("font-size").then(function (attr) {
@@ -60,9 +60,10 @@ test.describe('Проверка стиля в Chrome', function () {
                     assert.ok(attr.indexOf("through")>0, "Проверка - основная цена зачеркнута на карточке");
                     return attr; });
 
+
         })
 
-        // Аукционная цена продукта на карточке
+        // Акционная цена продукта на карточке
         driver.findElement(By.css("#box-campaigns > div > div > div > a > div.info > div.price-wrapper > strong"))
             .then(function (elem) {
                 elem.getAttribute("textContent").then(function (attr) {
@@ -71,16 +72,16 @@ test.describe('Проверка стиля в Chrome', function () {
 
                 elem.getCssValue("color").then(function (attr) {
                     var str = attr.split("(")[1].split(",",3);
-                    assert.ok( str[1]&&str[2]&&"0", "Проверка - цвет акционной цены на карточке");
+                    assert.ok( str[1]&&str[2]&&"0", "Проверка  цвет акционной цены на карточке");
                     return attr;});
 
                 elem.getCssValue("font-size").then(function (attr) {
                     salePriceSize = attr;
-                    assert.ok( salePriceSize > priceSize, "Проверка - размер шрифта акционной цены и основной цены на карточке");
+                    assert.ok( salePriceSize > priceSize, "Проверка - размер шрифта акционной цены и основной цены  на карточке");
                     return salePriceSize; });
 
                 elem.getCssValue("font-weight").then(function (attr) {
-                    assert.ok(attr =="bold", "Проверка - шрифт акционной цены bold на карточке");
+                    assert.ok(attr > 500, "Проверка - шрифт акционной цены bold на карточке");
                     return attr; });
             })
 
@@ -101,7 +102,7 @@ test.describe('Проверка стиля в Chrome', function () {
         driver.findElement(By.css("#box-product  del.regular-price"))
             .then(function (elem) {
                 elem.getAttribute("textContent").then(function (attr) {
-                    assert.equal(price, attr, "Проверка - основной цены продукта(карточка и страница)");
+                    assert.equal(price, attr, "Проверка - основной цены продукта (карточка и страница)");
                     return price; });
 
                 elem.getCssValue("color").then(function (attr) {
@@ -123,21 +124,21 @@ test.describe('Проверка стиля в Chrome', function () {
         driver.findElement(By.css("#box-product div.price-wrapper strong"))
             .then(function (elem) {
                 elem.getAttribute("textContent").then(function (attr) {
-                    assert.equal(salePrice, attr, "Проверка - акционной цены продукта(карточка и страница)");
+                    assert.equal(salePrice, attr, "Проверка - акционной цены продукта (карточка и страница)");
                     return salePrice; });
 
                 elem.getCssValue("color").then(function (attr) {
                     var str = attr.split("(")[1].split(",",3);
-                    assert.ok( str[1]&&str[2]&&"0", "Проверка  цвет акционной цены на странице");
+                    assert.ok( str[1]&&str[2]&&"0", "Проверка - цвет акционной цены на странице");
                     return attr;});
 
                 elem.getCssValue("font-size").then(function (attr) {
                     salePriceSize = attr;
-                    assert.ok( salePriceSize > priceSize, "Проверка - размер шрифта акционной цены и основной цены на странице");
+                    assert.ok( salePriceSize > priceSize, "Проверка - размер акционной цены больше основной цены на странице");
                     return salePriceSize; });
 
                 elem.getCssValue("font-weight").then(function (attr) {
-                    assert.ok(attr =="bold", "Проверка - шрифт акционной цены bold на странице");
+                    assert.ok(attr > 500, "Проверка - шрифт акционной цены bold на странице");
                     return attr; });
             })
 
--- LAB/less5_lab10IE.js
+++ LAB/less5_lab10IE.js
@@ -1,24 +1,28 @@
-// Задание 10
+// Задание 10 IE
 // Войти на главную страницу http://localhost/litecart
 // Проверить наименование товара, цены товара (обычная, аукционная)
 // Проверить стиль цены товара (обычная, аукционная)
 
 var webdriver = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
 var By = webdriver.By;
 var until = webdriver.until;
 var test = require('selenium-webdriver/testing');
 var assert = require('assert');
 var $q = require('q');
+ie = require('selenium-webdriver/ie');
 
-test.describe('Проверка стиля в Chrome', function () {
+test.describe('Проверка стиля в Firefox', function () {
     var driver;
 
     test.before(function () {
+        var options = new ie.Options();
+        options.requireWindowFocus(true)
+
         driver = new webdriver.Builder()
-            .withCapabilities({'unexpectedAlertBehaviour': 'dismiss'})
-            .forBrowser('chrome')
+            .forBrowser('ie')
+            .setIeOptions(options)
             .build();
+
 
         driver.get('http://localhost/litecart/');
         driver.manage().timeouts().implicitlyWait(10000/*ms*/);
@@ -49,7 +53,7 @@ test.describe('Проверка стиля в Chrome', function () {
 
                 elem.getCssValue("color").then(function (attr) {
                     var str = attr.split("(")[1].split(",",3);
-                    assert.ok( str[0]&&str[1]&&str[2], "Проверка - цвет основной цены на карточке");
+                    assert.ok( str[0]&&str[1]&&str[2], "Проверка - цвет основной цены  на карточке");
                     return attr; })
 
                 elem.getCssValue("font-size").then(function (attr) {
@@ -60,9 +64,10 @@ test.describe('Проверка стиля в Chrome', function () {
                     assert.ok(attr.indexOf("through")>0, "Проверка - основная цена зачеркнута на карточке");
                     return attr; });
 
+
         })
 
-        // Аукционная цена продукта на карточке
+        // Акционная цена продукта на карточке
         driver.findElement(By.css("#box-campaigns > div > div > div > a > div.info > div.price-wrapper > strong"))
             .then(function (elem) {
                 elem.getAttribute("textContent").then(function (attr) {
@@ -71,16 +76,16 @@ test.describe('Проверка стиля в Chrome', function () {
 
                 elem.getCssValue("color").then(function (attr) {
                     var str = attr.split("(")[1].split(",",3);
-                    assert.ok( str[1]&&str[2]&&"0", "Проверка - цвет акционной цены на карточке");
+                    assert.ok( str[1]&&str[2]&&"0", "Проверка  цвет акционной цены на карточке");
                     return attr;});
 
                 elem.getCssValue("font-size").then(function (attr) {
                     salePriceSize = attr;
-                    assert.ok( salePriceSize > priceSize, "Проверка - размер шрифта акционной цены и основной цены на карточке");
+                    assert.ok( salePriceSize > priceSize, "Проверка - размер шрифта акционной цены и основной цены  на карточке");
                     return salePriceSize; });
 
                 elem.getCssValue("font-weight").then(function (attr) {
-                    assert.ok(attr =="bold", "Проверка - шрифт акционной цены bold на карточке");
+                    assert.ok(attr > 500, "Проверка - шрифт акционной цены bold на карточке");
                     return attr; });
             })
 
@@ -94,7 +99,7 @@ test.describe('Проверка стиля в Chrome', function () {
         // Наименование продукта на странице
         var h = driver.findElement(By.css("#box-product > div > div > h1")).getText()
             .then(function (text) {
-                assert.ok(text ==goodName, "Проверка - наименование продукта (карточка и страница)");
+                assert.ok(text ==goodName, "Проверка - наименование продукта(карточка и страница)");
             })
 
         // Основная цена продукта на странице
@@ -123,21 +128,21 @@ test.describe('Проверка стиля в Chrome', function () {
         driver.findElement(By.css("#box-product div.price-wrapper strong"))
             .then(function (elem) {
                 elem.getAttribute("textContent").then(function (attr) {
-                    assert.equal(salePrice, attr, "Проверка - акционной цены продукта(карточка и страница)");
+                    assert.equal(salePrice, attr, "Проверка - акционной цены продукта (карточка и страница)");
                     return salePrice; });
 
                 elem.getCssValue("color").then(function (attr) {
                     var str = attr.split("(")[1].split(",",3);
-                    assert.ok( str[1]&&str[2]&&"0", "Проверка  цвет акционной цены на странице");
+                    assert.ok( str[1]&&str[2]&&"0", "Проверка - цвет акционной цены на странице");
                     return attr;});
 
                 elem.getCssValue("font-size").then(function (attr) {
                     salePriceSize = attr;
-                    assert.ok( salePriceSize > priceSize, "Проверка - размер шрифта акционной цены и основной цены на странице");
+                    assert.ok( salePriceSize > priceSize, "Проверка - размер акционной цены больше основной цены на странице");
                     return salePriceSize; });
 
                 elem.getCssValue("font-weight").then(function (attr) {
-                    assert.ok(attr =="bold", "Проверка - шрифт акционной цены bold на странице");
+                    assert.ok(attr > 500, "Проверка - шрифт акционной цены bold на странице");
                     return attr; });
             })
 
