ac6c722a4836e92eb5e7dfe8c282edec9cb77e90
Толмачева Марина <tolmacheva.ma@gmail.com>
2017-05-12 08:01:53 +1000

    Задание 7

--- LAB/Less4_Lab7.js
+++ LAB/Less4_Lab7.js
@@ -9,55 +9,78 @@ var By = webdriver.By;
 var until = webdriver.until;
 var test = require('selenium-webdriver/testing');
 var assert = require('assert');
+var $q = require('q');
 
 var userName = 'admin',
     password = 'admin';
 
 
-test.describe('Check Main Left menu', function() {
+test.describe('Check Main Left menu', function () {
     var driver;
 
-    test.before(function() {
+    test.before(function () {
+
+        var options = new chrome.Options();
         driver = new webdriver.Builder()
             .withCapabilities({'unexpectedAlertBehaviour': 'dismiss'})
             .forBrowser('chrome')
+            .setChromeOptions(options)
             .build();
 
         driver.get('http://localhost/litecart/admin/');
         driver.findElement(By.name('username')).sendKeys(userName);
         driver.findElement(By.name('password')).sendKeys(password);
         driver.findElement(By.xpath("//button[@name='login']")).click();
+        driver.manage().timeouts().implicitlyWait(10000);
     });
 
-    test.it('Проверяем', function() {
-        // Список меню
-        var leftMenus = driver.findElements(By.css("#app-"));
-
-        // Клик по меню
-        for (var i = 0; i < leftMenus.length; i++) {
-            leftMenus[i].Click();
-
-            // Проверяем заголовок
-            driver.isElementPresent(By.css("#main h1")).then(function () {
-                assert.ok(true, "");
-            });
-
-            //Список подменю
-            var subMenu = driver.findElements(By.css("li#app- .docs span.name"));
-
-            // Клик по подменю
-            for (var j = 0; j < subMenu.length; j++) {
-                var subName = subMenu[j].Click();
-
-                // Проверяем заголовок
-                driver.isElementPresent(By.css("#main h1")).then(function () {
-                    assert.ok(true, "");
-                });
-            };
-        };
-    });
-
-    test.after(function() {
+    test.after(function () {
         driver.quit();
+    });
+
+    test.it('Проверяем', function () {
+
+        // Список меню
+        driver.findElements(By.css("#app- span.fa-lg")).then(function (elemMenus) {
+            var i = 0;
+            var arrMenuClicks = [];
+
+            elemMenus.map(function (curElem) {
+                arrMenuClicks.push(
+                    driver.findElements(By.css("#app- span.fa-lg")).then(function (mainMenus) {
+                        mainMenus[i].click()
+                            .then(function (click) {
+                                    driver.sleep(500);
+                                    driver.findElement(By.css("h1")).getText().then(function (text) {
+                                        assert.ok( text.length>0, "Заголовок есть")
+                                    });
+                                    driver.findElements(By.css("#app-.selected .docs a"))
+                                        .then((subElems) => {
+                                            var arrSubMenuClicks = [];
+                                            var j = 0;
+                                            subElems.map(function (curSubElem) {
+                                                arrSubMenuClicks.push(
+                                                    driver.findElements(By.css("#app-.selected .docs a"))
+                                                        .then(function (subMenus) {
+                                                            subMenus[j++].click().then(function (click) {
+                                                                driver.findElement(By.css("h1")).getText().then(function (text) {
+                                                                    assert.ok( text.length>0, "Заголовок есть")
+                                                                });
+                                                            })
+                                                        })
+                                                );
+                                            });
+                                            $q.all(arrSubMenuClicks);
+                                        });
+                                },
+                                function (err) {
+                                    return false;
+                                })
+                        i++
+                    })
+                )
+            })
+            $q.all(arrMenuClicks);
+        });
     });
 });
