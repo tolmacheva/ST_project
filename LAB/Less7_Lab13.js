b01e59ac533c1ae0cd6bf2c316fc50457f522c77
Толмачева Марина <tolmacheva.ma@gmail.com>
2017-05-14 23:41:11 +1000

    Задание 13

---
+++ LAB/Less7_Lab13.js
@@ -0,0 +1,85 @@
+// Задание 12
+// Войти на страницу http://localhost/litecart/admin
+// Зайти в меню Каталог и добавить новый товар
+
+
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var By = webdriver.By;
+var until = webdriver.until;
+var test = require('selenium-webdriver/testing');
+var assert = require('assert');
+var Key = webdriver.Key;
+var $q = require('q');
+
+test.describe('Регистрация пользователя', function() {
+    var driver;
+
+    test.before(function() {
+        driver = new webdriver.Builder()
+            .withCapabilities({'unexpectedAlertBehaviour': 'dismiss'})
+            .forBrowser('chrome')
+            .build();
+
+        driver.get('http://localhost/litecart/');
+        driver.manage().timeouts().implicitlyWait(10000);
+    });
+
+    test.after(function() {
+        //driver.quit();
+    });
+
+    test.it('Добавляем товар', function() {
+
+        var inputField;
+        var i = 0;
+        var sizeProduct = ["Small", "Medium", "Large"]
+
+        driver.findElement(By.css("#box-campaigns > div > div > div")).click()
+            .then(
+                (click) => {
+                    driver.findElement(By.css("#site-menu li.general-0 > a")).click();
+                    return true;
+                },
+                (err) => {
+                    return false;
+                }
+            );
+
+        sizeProduct.map((curElem)=>{
+            driver.findElement(By.css("#box-campaigns > div > div > div")).then(function () {
+
+                driver.findElement(By.css("#box-campaigns > div > div > div")).click().then((click) => {
+
+                    inputField = driver.findElement(By.css("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div:nth-child(3) > select"))
+                    driver.wait(driver.executeScript("arguments[0].value = '" + curElem + "'", inputField), 1000);
+                    driver.findElement(By.css("#box-product  button[name = 'add_cart_product']")).click();
+
+                    driver.wait(until.elementTextContains(driver.findElement(By.css("#cart span.quantity")), i), 10000);
+
+                    driver.findElement(By.css("body > div.featherlight.active > div > ul > li:nth-child(1) > a")).click();
+                    driver.wait(until.elementIsVisible(driver.findElement(By.css("#box-campaigns > div > div > div"))), 10000);
+                });
+                i++;
+            });
+        });
+
+        driver.findElement(By.css("#cart > a")).click();
+        var arrDel =[];
+        driver.findElements(By.css("table tr td:nth-child(6) i.fa-trash"))
+            .then((Elems) =>{
+                Elems.map((curElem) => {
+                    arrDel.push(
+                        driver.findElements(By.css("table tr td:nth-child(6) i.fa-trash"))
+                            .then(function () {
+                                driver.findElement(By.css("table  tr:nth-child(1) > td:nth-child(6) i.fa-trash")).click()
+                                    .then((clickDel) => {
+                                        driver.wait(until.stalenessOf(driver.findElement(By.css("#box-checkout > div.summary.wrapper"))),1000);
+                                    });
+                            })
+                    )
+                });
+                $q.all(arrDel);
+            });
+    });
+});
