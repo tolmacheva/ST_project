7b1712fa00890d5d949a8ef8fa5c21c0d02c6cb9
Толмачева Марина <tolmacheva.ma@gmail.com>
2017-05-16 22:08:34 +1000

    Исправленное задание 11

--- LAB/Less6_Lab11.js
+++ LAB/Less6_Lab11.js
@@ -11,11 +11,15 @@ var until = webdriver.until;
 var test = require('selenium-webdriver/testing');
 var assert = require('assert');
 var Key = webdriver.Key;
-
+var $q=require('q');
 
 test.describe('Регистрация пользователя', function() {
-    var driver;
+    var driver; var email; var password;
 
+    var getRandomNumber = function () {
+        var n=100; var m=1000;
+        return Math.floor(Math.random( ) * (n - m + 1)) + m
+    };
     test.before(function() {
         driver = new webdriver.Builder()
             .withCapabilities({'unexpectedAlertBehaviour': 'dismiss'})
@@ -27,67 +31,56 @@ test.describe('Регистрация пользователя', function() {
 
     });
 
-
-    test.it('Регистрируемся', function() {
-
-        driver.findElement(By.css("#box-account-login > form > p > a")).click().then(
-            function (click) {
-
-                driver.findElement(By.name("firstname")).sendKeys("Marina" + Key.TAB
-                    + "Tolmacheva" + Key.TAB + "100 let Vladivostoky" + Key.TAB + "Davydova" + Key.TAB
-                    + "69006" + Key.TAB + "Vladivostok" + Key.TAB + "United States"
-                    + Key.TAB + "tolmacheva@list.ru" + Key.TAB + "+7 914 123 98 70" + Key.TAB
-                    + "1234" + Key.TAB + "1234" + Key.TAB);
-                driver.sleep(1000);
-
-                driver.findElement(By.css("#box-create-account > form select[name = 'country_code']")).getAttribute("value")
-                    .then(function (text) {
-                        if (text = "US") {
-                            driver.findElement(By.css("#box-create-account > form select[name = 'zone_code']")).click();
-                            driver.sleep(1000);
-                            driver.findElement(By.css("#box-create-account > form select[name = 'zone_code']"))
-                                    .sendKeys("Alaska" + Key.ENTER);
-                        }
-                    });
-
-                driver.findElement(By.css("#box-create-account > form input[name='newsletter']")).getAttribute("value")
-                    .then(function (text) {
-                        if (text = "1") {
-                            driver.findElement(By.css("#box-create-account > form input[name='newsletter']")).click();
-                            driver.sleep(500);
-                        }
-                    })
-            });
-
-
-        driver.findElement(By.css("#box-create-account > form button[name='create_account']")).click();
-
-        driver.findElement(By.linkText("Logout")).click();
-
-    });
-
-
-    test.it('Авторизация', function () {
-
-
-            driver.findElement(By.name("email")).sendKeys("tolmacheva2@list.ru" + Key.ENTER);
-            driver.findElement(By.name("password")).sendKeys("1234" + Key.ENTER);
-
-            driver.wait(function () {
-                driver.findElement(By.css("button[name = 'login'")).click();
-            }, 1000).then(
-            function (click) {
-                driver.sleep(1000);
-                driver.findElement(By.linkText("Logout")).click();
-                return true;
-            },
-            function (err) {
-                return false;
-            }
-        );
-    })
-
     test.after(function() {
         driver.quit();
     });
+
+    test.it('Регистрируемся', function() {
+
+        var inputField;
+        var number =getRandomNumber();
+        var firstname = "Marina";
+        var lastname = "Tolmacheva" + number;
+        email = "emailUser"+number+"@list.ru";
+        password = lastname;
+
+        driver.findElement(By.linkText("New customers click here")).click().then(
+            (click)=> {
+                driver.findElement(By.name("firstname")).sendKeys(firstname + Key.TAB
+                    + lastname + Key.TAB + "100 let Vladivostoky" + Key.TAB + "Davydova" + Key.TAB
+                    + "69006" + Key.TAB + "Vladivostok" + Key.TAB + "United States"
+                    + Key.TAB + email + Key.TAB + "+7 914 123 98 70" + Key.TAB
+                    + password + Key.TAB + password).then(()=>
+
+                driver.findElement(By.css("select[name = 'country_code']")).getAttribute("value")
+                    .then((text) =>{
+                        if (text == "US") {
+                            var selectElem = driver.findElement(By.css("select[name = 'zone_code']"));
+                            selectElem.click();
+                            selectElem.findElement(By.css("option[value='AK']")).click();
+                        }
+                    })).then(()=>
+                driver.findElement(By.css("input[name='newsletter']")).getAttribute("value")
+                    .then((text) =>{
+                        if (text == "1") {
+                            driver.findElement(By.css("input[name='newsletter']")).click();
+                        }
+                    })).then(()=>
+
+                driver.findElement(By.css("button[name='create_account']")).click()).then(()=>
+
+                driver.wait(driver.findElement(By.linkText("Logout")).click(),1000));
+            }
+        );
+    });
+
+    test.it('Авторизация', function () {
+
+            driver.findElement(By.name("email")).sendKeys(email).then(()=>
+            driver.findElement(By.name("password")).sendKeys(password)).then(()=>
+            driver.wait(driver.findElement(By.css("button[name = 'login'")).click(), 1000)).then(()=>
+
+            driver.wait(driver.findElement(By.linkText("Logout")).click(), 1000));
+    });
+
 });
