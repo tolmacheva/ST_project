41c42b59e1458f720e8ae3a94f9fee89ba5e7486
Толмачева Марина <tolmacheva.ma@gmail.com>
2017-05-17 21:38:24 +1000

    Исправление задания 13

--- LAB/Less7_Lab13.js
+++ LAB/Less7_Lab13.js
@@ -71,11 +71,13 @@ test.describe('Регистрация пользователя', function() {
                 Elems.map((curElem) => {
                     arrDel.push(
                         driver.findElements(By.css("table tr td:nth-child(6) i.fa-trash"))
-                            .then(function () {
-                                var checkElem = driver.findElement(By.css("#box-checkout > div.summary.wrapper"));
-                                driver.findElement(By.css("table  tr:nth-child(1) > td:nth-child(6) i.fa-trash")).click()
-                                    .then((clickDel) => {
-                                        driver.wait(until.stalenessOf(checkElem),1000);
+                            .then(() =>{
+                                driver.findElement(By.css("#box-checkout > div.summary.wrapper"))
+                                    .then((checkElem) => {
+                                        driver.findElement(By.css("table  tr:nth-child(1) > td:nth-child(6) i.fa-trash")).click()
+                                            .then((clickDel) => {
+                                                driver.wait(until.stalenessOf(checkElem), 1000);
+                                            })
                                     });
                             })
                     )
